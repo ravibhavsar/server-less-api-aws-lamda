08a6698600e078acd407d72a32f1b23f587e7262
Ravi Bhavsar <ravi.bhavsar@Whitechsolutions.com>
2017-06-02 10:00:17 +1000

    fixing test for return signed url

--- web-api/spec/webSpec.js
+++ web-api/spec/webSpec.js
@@ -2,40 +2,8 @@ describe("web Server", function() {
         it("return signed url", function() {
                 var AWS = require('aws-sdk');
                 var s3 = new AWS.S3({ useAccelerateEndpoint: true, signatureVersion: 'v4' });
-
-                var params = {
-                    Bucket: "team.smart.imagine",
-                    Key: 'abc',
-                    ContentType: 'jpeg',
-                    ACL: 'public-read'
-                };
-
-                var signedUrl = new Promise(function(resolve, reject) {
-                    s3.getSignedUrl("putObject", params, function(error, url) {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            resolve(url);
-                        }
-                    });
-                });
-
-                var baseSignedUrl;
-                signedUrl.then(function(result) {
-                    //console.log(result.split('?'));
-                    this.baseSignedUrl = result.split('?');
-                });
-
-                var expectedBaseSignedUrl = 'https://s3.amazonaws.com/team.smart.imagine/abc';
-                //expect(this.baseSignedUrl[0]).toBe(expectedBaseSignedUrl);
-
-        });
-
-                it("return signed url11", function() {
-                                    var AWS = require('aws-sdk');
-                var s3 = new AWS.S3({ useAccelerateEndpoint: true, signatureVersion: 'v4' });
                     var objectName = 'abc';
-                    var fileName = (Math.floor(Math.random() * (1000000000 - 100 + 1)) + 100) +'.jpeg';
+                    var fileName = 'test.jpeg';
                     console.log(fileName);
                     var params = {
                         Bucket: "team.smart.imagine",
@@ -44,19 +12,18 @@ describe("web Server", function() {
                         ACL: 'public-read'
                     };
                     
-                    var respone =  new Promise(function(resolve, reject) {
-                        s3.getSignedUrl("putObject", params, function(error, url) {
-                            if (error) {
-                                reject(error);
-                            } else {
-                                resolve ({
-                                    signedUrl: url,
-                                });
-                            }
-                        });
+                    var signedUrl = null;
+                    s3.getSignedUrl("putObject", params, function(error, url) {
+                                    if (error) {
+                                        reject(error);
+                                    } else {
+                                        signedUrl = url;
+                                    }
                     });
-
-                    console.log(respone);
+                    
+                    var baseSignedUrl= (signedUrl.split('?'))[0];
+                    var expectedBaseSignedUrl = 'https://s3.amazonaws.com/team.smart.imagine/in/abc/test.jpeg';
+                    expect(expectedBaseSignedUrl).toBe(baseSignedUrl);
 
         });
 
